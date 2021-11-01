f46a37e8e2d5abe7b8b7199219747d471bf96c21
Antoine Barthelemy <antoine@linux.com>
2021-11-01 13:52:37 +0100

    feat(p1): multiple host

--- p1/bootstrap.sh
+++ p1/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
-dnf update
-dnf install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
-fi
-
+echo OK!
+#dnf update
+#dnf install -y apache2
+#if ! [ -L /var/www ]; then
+#  rm -rf /var/www
+#  ln -fs /vagrant /var/www
+#fi
+#
