6a5ea9991542a85f400a33f316869bac30f8bf51
Antoine Barthelemy <antoine@linux.com>
2021-11-01 17:09:07 +0100

    update(script): install cross Os

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -e
-sudo dnf install -y vagrant
-exit 0
-# https://www.virtualbox.org/wiki/Linux_Downloads
-VERSION='6.1.28'
-PKG="VirtualBox-6.1-${VERSION}_147628_fedora33-1.x86_64.rpm"
-TMP='/tmp'
-[ -e ${TMP}/${PKG} ] || wget https://download.virtualbox.org/virtualbox/${VERSION}/${PKG} -O ${TMP}/${PKG}
-sudo dnf install -y ${TMP}/${PKG}
+_package_manager_detect() {
+	command -v apt &>/dev/null && pk="apt" && return
+	command -v yum &>/dev/null && pk="yum" && return
+	_error "No supported package manager installed on system"
+	_error "(supported: apt or yum)"
+	exit 1
+}
+_package_manager_detect
+sudo ${pk} update
+sudo ${pk} install vagrant
+#VERSION='6.1.28'
+#PKG="VirtualBox-6.1-${VERSION}_147628_fedora33-1.x86_64.rpm"
+#TMP='/tmp'
+#[ -e ${TMP}/${PKG} ] || wget https://download.virtualbox.org/virtualbox/${VERSION}/${PKG} -O ${TMP}/${PKG}
+#sudo ${pk} install ${TMP}/${PKG}
