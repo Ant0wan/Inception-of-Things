09ca829e8fff25fda3f0d952fce170271b191506
Antoine Barthelemy <antoine@linux.com>
2021-11-03 23:08:26 +0100

    update(p1): k3s on the go

--- p1/k3s.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#curl -sLS https://get.k3sup.dev | sh
-#sudo install k3sup /usr/local/bin/
-#rm ./k3sup
-#sudo echo "192.168.42.110 localhost"
-#k3sup install --ip 192.168.42.110 --user root
