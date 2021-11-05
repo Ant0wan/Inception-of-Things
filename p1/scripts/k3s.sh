4332f827ff35a131ab59715362f144f3fd4ea4bf
Antoine Barthelemy <antoine@linux.com>
2021-11-05 23:26:17 +0100

    feat(p1): k3s with var script

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
+export PATH=$PATH:/usr/local/bin/
 mkdir -p /etc/rancher/k3s/
 curl -sfL https://get.k3s.io | sh -s -
+cp /vagrant/confs/$1.yaml /etc/rancher/k3s/$1.yaml
+if [ $1 == "server" ]; then
+	k3s server &
+	cp /etc/rancher/k3s/k3s.yaml /vagrant/
+	cp /var/lib/rancher/k3s/server/node-token /vagrant/
+	echo "I am a server"
+else
+	#k3s agent -s https://server.k3s.local:6443 -t $(cat /vagrant/token) --with-node-id 1
+	echo "I am an agent"
+fi
