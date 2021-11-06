28aaab158f7b0ce7046191bcac8279a76d059979
Antoine Barthelemy <antoine@linux.com>
2021-11-06 11:27:38 +0100

    feat(script): simplify script

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -3,6 +3,6 @@ export PATH=$PATH:/usr/local/bin/
 mkdir -p /etc/rancher/k3s/
 curl -sfL https://get.k3s.io | sh -s -
 cp /vagrant/confs/$1.yaml /etc/rancher/k3s/$1.yaml
-k3s $1 &
+k3s &
 if [ $1 == "server" ]; then cp /var/lib/rancher/k3s/server/node-token /vagrant/; fi
 #cp /etc/rancher/k3s/k3s.yaml /vagrant/
