49d584b671e03c91b57dcc7d193d4107469764ee
Antoine Barthelemy <antoine@linux.com>
2021-11-07 16:42:54 +0100

    test(p1): copy conig before script start

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 export PATH=$PATH:/usr/local/bin/
 mkdir -p /etc/rancher/k3s/
-curl -sfL https://get.k3s.io | sh -s -
 cp /vagrant/confs/$1.yaml /etc/rancher/k3s/$1.yaml
+curl -sfL https://get.k3s.io | sh -s -
 if [ $1 == "server" ]; then
-	k3s server &
 	cp /var/lib/rancher/k3s/server/node-token /vagrant/
 	cp /etc/rancher/k3s/k3s.yaml /vagrant/
 	echo "alias k='/usr/local/bin/k3s kubectl'" >> /root/.bashrc
-else
-	k3s agent -s $2 -t $(cat /vagrant/node-token)
 fi
