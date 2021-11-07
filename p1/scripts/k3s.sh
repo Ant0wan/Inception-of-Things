f0c0285abef844d530c1f6b926f13ff9177a26da
Antoine Barthelemy <antoine@linux.com>
2021-11-07 15:36:17 +0100

    fix

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -4,10 +4,10 @@ mkdir -p /etc/rancher/k3s/
 curl -sfL https://get.k3s.io | sh -s -
 cp /vagrant/confs/$1.yaml /etc/rancher/k3s/$1.yaml
 if [ $1 == "server" ]; then
-	k3s $1 &
+	k3s server &
 	cp /var/lib/rancher/k3s/server/node-token /vagrant/
+	cp /etc/rancher/k3s/k3s.yaml /vagrant/
 	echo "alias k='/usr/local/bin/k3s kubectl'" >> /root/.bashrc
 else
-	k3s $1
+	k3s agent -s $2 -t $(cat /vagrant/node-token)
 fi
-#cp /etc/rancher/k3s/k3s.yaml /vagrant/
