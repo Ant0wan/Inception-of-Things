a20f9f2bd851fff7189ea47a631e3f48e83113d5
Antoine Barthelemy <antoine@linux.com>
2021-11-07 19:17:31 +0100

    test

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -5,6 +5,11 @@ cp /vagrant/confs/$1.yaml /etc/rancher/k3s/config.yaml
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=$1 sh -
 if [ -e /var/lib/rancher/k3s/server/node-token ]; then
 	cp -n /var/lib/rancher/k3s/server/node-token /vagrant/
+	cat << ALIAS | sudo tee -a /root/.bashrc
+export PATH=$PATH:/usr/local/bin/
+alias k='/usr/local/bin/k3s kubectl'
+alias kubectl='/usr/local/bin/k3s kubectl'
+ALIAS
 fi
 if [ -e /etc/rancher/k3s/k3s.yaml ]; then
 	cp -n /etc/rancher/k3s/k3s.yaml /vagrant/
