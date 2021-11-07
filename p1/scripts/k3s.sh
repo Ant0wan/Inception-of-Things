16dfa6e885aedf5b8e25bcf48607ceaa30f6b583
Antoine Barthelemy <antoine@linux.com>
2021-11-07 13:37:00 +0100

    feat(p1): end of story

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -3,6 +3,8 @@ export PATH=$PATH:/usr/local/bin/
 mkdir -p /etc/rancher/k3s/
 curl -sfL https://get.k3s.io | sh -s -
 cp /vagrant/confs/$1.yaml /etc/rancher/k3s/$1.yaml
-k3s &
-if [ $1 == "server" ]; then cp /var/lib/rancher/k3s/server/node-token /vagrant/; fi
+k3s $1 &
+if [ $1 == "server" ]; then
+	cp /var/lib/rancher/k3s/server/node-token /vagrant/
+fi
 #cp /etc/rancher/k3s/k3s.yaml /vagrant/
