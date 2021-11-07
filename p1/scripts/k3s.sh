d6b3d474b049f2ded6ab1ec98134b72ca821b2e4
Antoine Barthelemy <antoine@linux.com>
2021-11-07 15:17:33 +0100

    feat(script): simplify script

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -3,8 +3,11 @@ export PATH=$PATH:/usr/local/bin/
 mkdir -p /etc/rancher/k3s/
 curl -sfL https://get.k3s.io | sh -s -
 cp /vagrant/confs/$1.yaml /etc/rancher/k3s/$1.yaml
-k3s $1 &
 if [ $1 == "server" ]; then
+	k3s $1 &
 	cp /var/lib/rancher/k3s/server/node-token /vagrant/
+	echo "alias k='/usr/local/bin/k3s kubectl'" >> /root/.bashrc
+else
+	k3s $1
 fi
 #cp /etc/rancher/k3s/k3s.yaml /vagrant/
