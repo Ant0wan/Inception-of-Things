3b37f4e315b526378501b541daed74dc19213539
Antoine Barthelemy <antoine@linux.com>
2021-11-07 19:08:25 +0100

    test(p1): copy conig before script start

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/usr/local/bin/
 mkdir -p /etc/rancher/k3s/
 cp /vagrant/confs/$1.yaml /etc/rancher/k3s/config.yaml
-curl -sfL https://get.k3s.io | sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=$1 sh -
 if [ -e /var/lib/rancher/k3s/server/node-token ]; then
 	cp -n /var/lib/rancher/k3s/server/node-token /vagrant/
 fi
