2d26d967adb6dcc8dd464a6f7b65393db4042936
Antoine Barthelemy <antoine@linux.com>
2021-11-08 21:53:21 +0100

    update(p2): kustomize install clean

--- p2/scripts/kustomize.sh
+++ p2/scripts/kustomize.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 sudo install ./kustomize /usr/bin
+rm ./kustomize
