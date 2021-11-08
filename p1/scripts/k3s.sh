d2909a393d2258f46addbfef1853ee64bdf13d75
Antoine Barthelemy <antoine@linux.com>
2021-11-08 21:39:14 +0100

    test(p1 p2): nat on 2

--- p1/scripts/k3s.sh
+++ p1/scripts/k3s.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 export PATH=$PATH:/usr/local/bin/
-#systemctl disable firewalld --now
 mkdir -p /etc/rancher/k3s/
 cp /vagrant/confs/$1.yaml /etc/rancher/k3s/config.yaml
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=$1 sh -
