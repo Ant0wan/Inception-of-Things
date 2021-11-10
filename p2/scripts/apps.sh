30d9d44b1bbe6ffaff941c18527e520a80a94c23
Antoine Barthelemy <antoine@linux.com>
2021-11-10 17:08:59 +0100

    feat(p2): app all same like in subject

--- p2/scripts/apps.sh
+++ p2/scripts/apps.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-cd /vagrant/confs/apps/
-kustomize build > /tmp/$1.yaml
-envsubst < /tmp/$1.yaml | kubectl apply -f -
+cd /vagrant/confs/apps/base
+kustomize build > /tmp/$1-base.yaml
+envsubst < /tmp/$1-base.yaml | kubectl apply -f -
+cd /vagrant/confs/apps/overlays/$1
+kustomize build > /tmp/$1-overlays.yaml
