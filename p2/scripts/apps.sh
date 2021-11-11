ac68845cdb3dcc1dcceb5c7b191f7b7def71c461
Antoine Barthelemy <antoine@linux.com>
2021-11-11 14:07:49 +0100

    refacto(p2): ingress and all manifests

--- p2/scripts/apps.sh
+++ p2/scripts/apps.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-set -ex
 cd /vagrant/confs/apps/base
 kustomize build | envsubst > /tmp/$1.yaml
 cd /vagrant/confs/apps/overlays/
 if [ $1 == "app-three" ]; then
 	envsubst < default-ingress.yaml >> /tmp/$1.yaml
-#	kubectl apply -f /tmp/$1.yaml
 else
 	envsubst < ingress.yaml >> /tmp/$1.yaml
-#	kubectl apply -f /tmp/$1.yaml
 fi
+kubectl apply -f /tmp/$1.yaml
