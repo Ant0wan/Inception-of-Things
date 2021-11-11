b75971bf8226aedadc2ab9848f2377c3af85a42b
Antoine Barthelemy <antoine@linux.com>
2021-11-11 16:49:29 +0100

    test script installing k3d

--- scripts/k3d.sh
+++ scripts/k3d.sh
@@ -5,3 +5,12 @@ if command -v wget; then
 elif command -v curl; then
 	curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 fi
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
