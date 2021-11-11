d94fbbffeb05576a6fdf0993531efc6de6f16ea8
Antoine Barthelemy <antoine@linux.com>
2021-11-11 13:20:04 +0100

    refacto(p2): app deployment

--- p2/scripts/apps.sh
+++ p2/scripts/apps.sh
@@ -2,6 +2,11 @@
 cd /vagrant/confs/apps/base
 kustomize build > /tmp/$1-base.yaml
 envsubst < /tmp/$1-base.yaml | kubectl apply -f -
-cd /vagrant/confs/apps/overlays/$1
-kustomize build > /tmp/$1-overlays.yaml
-envsubst < /tmp/$1-overlays.yaml | kubectl apply -f -
+cd /vagrant/confs/apps/overlays/
+if [ $1 == "app-three" ]; then
+	envsubst < default-ingress.yaml > /tmp/$1-overlays.yaml
+	kubectl apply -f /tmp/$1-overlays.yaml
+else
+	envsubst < ingress.yaml > /tmp/$1-overlays.yaml
+	kubectl apply -f /tmp/$1-overlays.yaml
+fi
