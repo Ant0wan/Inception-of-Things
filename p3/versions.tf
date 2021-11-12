114f548c254a69d99c7c15e72a8711331dcc0a10
Antoine Barthelemy <antoine@linux.com>
2021-11-12 14:18:25 +0100

    feat(p3): deploy k3d cluster

--- p3/versions.tf
+++ p3/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.6.1"
+    }
   }
   required_version = ">= 0.13"
 }
