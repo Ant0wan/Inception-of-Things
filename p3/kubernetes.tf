e25f646985af22415cf9bb9cb41a1dac9fa14151
Antoine Barthelemy <antoine@linux.com>
2021-11-12 23:05:08 +0100

    feat(p3): adding kubernetes module

--- p3/kubernetes.tf
+++ p3/kubernetes.tf
@@ -9,3 +9,63 @@ resource "kubernetes_namespace" "p3_namespaces" {
    null_resource.cluster
  ]
 }
+
+resource "kubernetes_deployment" "p3_deployments" {
+  metadata {
+    name = "name"
+    labels = {
+      test = "MyExampleApp"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/nginx_status"
+              port = 80
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "Awesome"
+              }
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
