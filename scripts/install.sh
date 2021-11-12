2885997e407da667415c3cbabb156ce50966043c
Antoine Barthelemy <antoine@linux.com>
2021-11-12 15:24:39 +0100

    feat(p3): install script for k3d

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-export VERSION='6.1.28'
-export TMP='/tmp'
 _package_manager_detect() {
 	command -v apt &>/dev/null && export pk="apt" && return
 	command -v yum &>/dev/null && export pk="yum" && return
@@ -10,12 +8,4 @@ _package_manager_detect() {
 	exit 1
 }
 _package_manager_detect
-#source virtualbox.sh
-source k3d.sh
-#	command -v apt &>/dev/null && pk="apt" && PKG="virtualbox-6.1_${VERSION}-147628~Ubuntu~eoan_amd64.deb" && return
-#	command -v yum &>/dev/null && pk="yum" && PKG="VirtualBox-6.1-${VERSION}_147628_fedora33-1.x86_64.rpm" && return
-#echo ${pk} $PKG
-#sudo ${pk} update
-#sudo ${pk} install vagrant
-#[ -e ${TMP}/${PKG} ] || wget https://download.virtualbox.org/virtualbox/${VERSION}/${PKG} -O ${TMP}/${PKG}
-#sudo ${pk} install ${TMP}/${PKG}
+source virtualbox.sh
--- scripts/libvirt.sh
+++
@@ -1,1 +0,0 @@
-https://developer.fedoraproject.org/tools/vagrant/vagrant-libvirt.html
