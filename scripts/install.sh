cacb75cc3a6f156669c6176e47e5f102e1d18e0a
Antoine Barthelemy <antoine@linux.com>
2021-11-12 11:06:14 +0100

    feat(p3): install script for k3d

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,15 +3,17 @@ set -e
 VERSION='6.1.28'
 TMP='/tmp'
 _package_manager_detect() {
-	command -v apt &>/dev/null && pk="apt" && PKG="virtualbox-6.1_${VERSION}-147628~Ubuntu~eoan_amd64.deb" && return
-	command -v yum &>/dev/null && pk="yum" && PKG="VirtualBox-6.1-${VERSION}_147628_fedora33-1.x86_64.rpm" && return
+	command -v apt &>/dev/null && pk="apt" && return
+	command -v yum &>/dev/null && pk="yum" && return
 	_error "No supported package manager installed on system"
 	_error "(supported: apt or yum)"
 	exit 1
 }
 _package_manager_detect
-echo ${pk} $PKG
-sudo ${pk} update
-sudo ${pk} install vagrant
-[ -e ${TMP}/${PKG} ] || wget https://download.virtualbox.org/virtualbox/${VERSION}/${PKG} -O ${TMP}/${PKG}
-sudo ${pk} install ${TMP}/${PKG}
+#	command -v apt &>/dev/null && pk="apt" && PKG="virtualbox-6.1_${VERSION}-147628~Ubuntu~eoan_amd64.deb" && return
+#	command -v yum &>/dev/null && pk="yum" && PKG="VirtualBox-6.1-${VERSION}_147628_fedora33-1.x86_64.rpm" && return
+#echo ${pk} $PKG
+#sudo ${pk} update
+#sudo ${pk} install vagrant
+#[ -e ${TMP}/${PKG} ] || wget https://download.virtualbox.org/virtualbox/${VERSION}/${PKG} -O ${TMP}/${PKG}
+#sudo ${pk} install ${TMP}/${PKG}
