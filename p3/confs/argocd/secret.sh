dfd17911b23aa9c14c55e9cac2a8796df12635f9
Antoine Barthelemy <antoine@linux.com>
2021-11-17 23:20:18 +0100

    feat(p3): argocd deploy ok, still dev app to do

--- p3/confs/argocd/secret.sh
+++
@@ -1,6 +0,0 @@
-# admin.password: camesaoul
-kubectl -n argocd patch secret argocd-secret \
-  -p '{"stringData": {
-    "admin.password": "$2a$12$ruDmhYZb.qDpBTeV9fMav.jBsGQer3TTOYZuGmBQAYZmycdnQhzOW",
-    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
-  }}'
