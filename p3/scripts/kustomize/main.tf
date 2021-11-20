6083ada5d1c4deadd9a242dd0146c8b7a0c5655e
Antoine Barthelemy <antoine@linux.com>
2021-11-21 00:37:17 +0100

    feat(p3): adding dev app to kustomize

--- p3/scripts/kustomize/main.tf
+++ p3/scripts/kustomize/main.tf
@@ -4,7 +4,7 @@ provider "kustomization" {
 
 locals {
   label_key = "app.kubernetes.io/part-of"
-  label_value = "argocd"
+  label_value = ["argocd", "dev"]
 
   time_ts = formatdate("YYYY-MM-DDThh:mm:ssZZZ", timestamp())
 
@@ -16,7 +16,7 @@ data "kustomization_overlay" "argocd" {
   namespace = "argocd"
 
   common_labels = {
-    (local.label_key) = local.label_value
+    (local.label_key) = local.label_value[0]
   }
 
   resources = [
@@ -26,38 +26,32 @@ data "kustomization_overlay" "argocd" {
   kustomize_options = {
     load_restrictor = "none"
   }
-
-#  patches {
-#    patch = <<-EOF
-#      - op: add
-#        path: data/server.insecure
-#        value: true
-#    EOF
-#    target = {
-#      version = "v1"
-#      kind = "ConfigMap"
-#      name = "argocd-cmd-params-cm"
-#      namespace = local.namespace
-#    }
-#  }
-#  patches {
-### password: faitchierlapedago
-#    patch = <<-EOF
-#      - op: replace
-#        path: stringData/admin.password
-#        value: "$2a$12$XC7M9hTkoNzfS.Pbwl72MOIimmSZ1qnWrxTWi1wOgpJzGSudi0dpS"
-#    EOF
-#    target = {
-#      version = "v1"
-#      kind = "Secret"
-#      name = "argocd-secret"
-#      namespace = local.namespace
-#    }
-#  }
 }
 
 resource "kustomization_resource" "argocd" {
   for_each = data.kustomization_overlay.argocd.ids
 
   manifest = data.kustomization_overlay.argocd.manifests[each.value]
+}
+
+data "kustomization_overlay" "dev" {
+  namespace = "dev"
+
+  common_labels = {
+    (local.label_key) = local.label_value[1]
+  }
+
+  resources = [
+    "../../confs/dev/"
+  ]
+
+  kustomize_options = {
+    load_restrictor = "none"
+  }
+}
+
+resource "kustomization_resource" "dev" {
+  for_each = data.kustomization_overlay.dev.ids
+
+  manifest = data.kustomization_overlay.dev.manifests[each.value]
 }
