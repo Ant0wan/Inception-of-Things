423a49a42daf879dc1628a0be31cf387a5abbcc4
Antoine Barthelemy <antoine@linux.com>
2021-12-19 15:04:13 +0100

    update(bonus): fmt and to json env file

--- bonus/main.tf
+++ bonus/main.tf
@@ -1,18 +1,18 @@
 locals {
-  raw_data     = jsondecode(file(pathexpand("../p3/scripts/terraform.tfvars.json")))
-  cluster = local.raw_data.cluster
+  raw_data = jsondecode(file(pathexpand("../p3/scripts/terraform.tfvars.json")))
+  cluster  = local.raw_data.cluster
 }
 
 module "k3d" {
-	source = "../p3/scripts/k3d/"
-	cluster = local.cluster
+  source  = "../p3/scripts/k3d/"
+  cluster = local.cluster
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = module.k3d.kube_config
-  }
-}
+#provider "helm" {
+#  kubernetes {
+#    config_path = module.k3d.kube_config
+#  }
+#}
 
 
 
--- bonus/script/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-name       = "gitlab"
-repository = "https://charts.bitnami.com/bitnami"
-chart      = "nginx-ingress-controller"
-service    = ["service.type", "ClusterIP"]
