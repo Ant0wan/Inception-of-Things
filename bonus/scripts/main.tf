d120f3a6619e259ba696db14ee0d9c361fb8acb5
Antoine Barthelemy <antoine@linux.com>
2021-12-22 23:43:00 +0100

    update(helm)

--- bonus/scripts/main.tf
+++ bonus/scripts/main.tf
@@ -1,3 +1,8 @@
+module "k3d" {
+  source  = "../../p3/scripts/k3d/"
+  cluster = local.cluster
+}
+
 resource "local_file" "kube_config" {
   content  = module.k3d.kube_config
   filename = "${path.module}/kube_config"
@@ -7,11 +12,6 @@ provider "helm" {
   kubernetes {
     config_path = local_file.kube_config.filename
   }
-}
-
-module "k3d" {
-  source  = "../../p3/scripts/k3d/"
-  cluster = local.cluster
 }
 
 locals {
